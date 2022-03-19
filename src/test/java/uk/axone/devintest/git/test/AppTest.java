9f42cd192ea98328a7d2c79d56ad5fd7255f6c48
ramduvvuri <ram.duvvuri@gmail.com>
2022-03-19 08:20:22 +0000

    Jira 1234 - Committing the project to Git Repo

---
+++ src/test/java/uk/axone/devintest/git/test/AppTest.java
@@ -0,0 +1,28 @@
+package uk.axone.devintest.git.test;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Running test 4");
+    }
+
+
+}
